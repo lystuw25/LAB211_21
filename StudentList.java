aba4dc0389d770bebacc9c420805886b8121626b
Co-khi-nao-em-tin-vao-loi-noi-doi <143399756+Co-khi-nao-em-tin-vao-loi-noi-doi@users.noreply.github.com>
2024-06-07 22:09:08 +0700

    Update StudentList.java

--- StudentList.java
+++ StudentList.java
@@ -1,10 +1,37 @@
+
 import java.util.*;
+
 public class StudentList {
+
     private ArrayList<Student> studentList = new ArrayList<>();
-    Scanner sc = new Scanner(System.in);
-    public void addStudent(Student s){
-        studentList.add(s);
+    private Validation v = new Validation();
+    private Scanner sc = new Scanner(System.in);
+    String p1 = "Enter the id: ";
+    String p2 = "Enter the name: ";
+    String p3 = "Enter the semester: ";
+    String p4 = "Enter the course (Java, .Net, C/C++): ";
+
+    public void add() throws Exception {
+        int size = 0;
+        while (true) {
+            while (true) {
+                Student s = new Student();
+                s.setId(v.checkInputInt(p1));
+                s.setStudentName(v.checkInputString(p2));
+                s.setSemester(v.checkInputInt(p3));
+                s.setCourseName(v.checkInputString(p4));
+                if (isValidStudent(s)) {
+                    studentList.add(s);
+                    System.out.println("Add success.");
+                    size++;
+                }
+                if (size >= 4) break;
+            }
+            System.out.print("Do you want to continue(Y/N)?: ");
+            if (!v.checkYN()) break;
+        }
     }
+
     public Student findByID(int id) {
         for (Student s : studentList) {
             if (s.getId() == id) {
@@ -12,82 +39,92 @@ public class StudentList {
             }
         }
         return null;
-    }    
-    public boolean checkValidStudent(Student s1){
+    }
+    public boolean isValidStudent(Student s1){
         for (Student s : studentList) {
-            if(s.equals(findByID(s1.getId())) ||
-              (s.getId()==s1.getId() && !s.getStudentName().equalsIgnoreCase(s1.getStudentName()))){
-                return false;
-            }
+            if(s.getId() == s1.getId()) return false;
         }
         return true;
     }
-    public void sort(){
+    
+
+    public void findAndSort() throws Exception {
+        String findName;
+        while (true) {
+            try {
+                findName = v.checkInputString(p2);
+                break;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        StudentList a = findByPart(findName);
+        if (a == null) {
+            System.out.println("Not found.");
+        } else {
+            System.out.printf("%-5s|%-15s|%-10s|%-10s\n", "ID", "Name", "Semester", "Course");
+            a.sort();
+            a.displayStudent();
+        }
+    }
+
+    public void sort() {
         Collections.sort(studentList, Comparator.comparing(Student::getStudentName));
     }
-    public StudentList findByPart(String name){
+
+    public StudentList findByPart(String name) {
         StudentList result = new StudentList();
         boolean check = true;
         for (Student s : studentList) {
-            if(s.getStudentName().contains(name)){
-                result.addStudent(s);
+            if (s.getStudentName().contains(name)) {
+                result.studentList.add(s);
                 check = false;
             }
         }
-        if(!check) return result;
+        if (!check) return result;
         else return null;
-    }   
-    public void displayStudent(){
+    }
+
+    public void updOrDel() throws Exception {
+        int findNum = v.checkInputInt(p1);
+        Student b = findByID(findNum);
+        if (b == null) {
+            System.out.println("Not found.");
+        } else {
+            System.out.print("Do you want to update (U) or delete (D) student? ");
+            if (v.checkUD()) {
+                b.setStudentName(v.checkInputString(p2));
+                b.setSemester(v.checkInputInt(p3));
+                b.setCourseName(v.checkInputString(p4));
+                System.out.println("Update success.");
+            } else {
+                studentList.remove(b);
+                System.out.println("Delele success.");
+            }
+        }
+    }
+
+    public void displayStudent() {
         for (Student s : studentList) {
             s.display();
         }
-    }   
-    public void delete(int id){
-        for (Student s : studentList) {
-            if(s.equals(findByID(id))){
-                studentList.remove(s);
-                break;
-            }
-        }
     }
-    public void updateStudent(Student s1) throws Exception{
-        for (Student s : studentList) {
-            if(s.equals(findByID(s1.getId()))){
-                s.setStudentName(s1.getStudentName());
-                s.setSemester(s1.getSemester());
-                s.setCourseName(s.getCourseName());
-                break;
-            }
-            
-        }
-    }
-    public boolean checkValidUpdate(Student s1, int id){
-        for (Student s : studentList) {
-            if(s.equals(findByID(id))) continue;
-            if(s.equals(findByID(s1.getId())) ||
-              (s.getId() == s1.getId() && !s.getStudentName().equalsIgnoreCase(s1.getStudentName()))){
-                return false;
-            }
-        }
-        return true;
-    }
-    public Map<String, Map<String, Integer>> reportStudent(){
+
+    public void displayReport() {
         Map<String, Map<String, Integer>> report = new LinkedHashMap<>();
         for (Student s : studentList) {
             String name = s.getStudentName();
             String course = s.getCourseName();
-            if(!report.containsKey(name)){
+            if (!report.containsKey(name)) {
                 report.put(name, new HashMap<>());
             }
             Map<String, Integer> courses = report.get(name);
-            if(!courses.containsKey(course)){
-                courses.put(course,0);
+            if (!courses.containsKey(course)) {
+                courses.put(course, 0);
             }
             courses.put(course, courses.get(course) + 1);
         }
-        return report;
-    }
-    public static void displayReport(Map<String, Map<String, Integer>> report){
+        System.out.printf("%-15s|%-10s|%-10s\n", "Name", "Course", "Total Course");
         for (String nameKey : report.keySet()) {
             Map<String, Integer> courses = report.get(nameKey);
             for (String courseKey : courses.keySet()) {
@@ -96,4 +133,5 @@ public class StudentList {
             }
         }
     }
+
 }
